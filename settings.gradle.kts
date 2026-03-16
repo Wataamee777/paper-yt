53fc0331394fa6887d40602db90f74a53d34676c
わたあめえ <Wataamee777@gmail.com>
2026-03-16 17:57:14 +0900

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,7 +14,11 @@ dependencyResolutionManagement {
         // ↓ これを追加！
         maven { url = uri("https://www.jitpack.io") }
     }
+    dependencies {
+	        implementation("com.github.arthenica:ffmpeg-kit:v6.0.LTS")
+	}
 }
+
 
 rootProject.name = "PaperYt"
 include(":app")
